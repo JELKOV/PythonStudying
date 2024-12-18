55f719038115fa049c70c6868f89c3b5bd26002b
JAEHO AHN <ajh4234@gmail.com>
2024-12-18 17:43:42 +0900

    Day 4: 가위바위보 게임 및 리스트 학습

---
+++ Day 4/Lists/task.py
@@ -0,0 +1,8 @@
+state_of_america = ["Delaware", "Pennsylvania"]
+print(state_of_america[0])
+
+state_of_america.extend(["Seoul", "Busan"])
+print(state_of_america)
+
+state_of_america.append("Daegu")
+print(state_of_america)
