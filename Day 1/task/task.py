f87a47962e5d058dd2376fd50be05417f437e8e0
JAEHO AHN <ajh4234@gmail.com>
2024-12-18 20:10:27 +0900

    Day 1: Band Name Generator 및 기본 입력 학습

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,4 +1,11 @@
-print("Welcome to the Band Name Generator.")
-city = input("Which city did you grow up in?\n")
-pet = input("What is the name of a pet?\n")
-print("Your band name could be: " + city + " " + pet)
+# 환영 메시지를 출력합니다.
+print("Welcome to the Band Name Generator.")  
+
+# 사용자에게 도시 이름을 입력받고, 입력값을 변수 'city'에 저장합니다.
+city = input("Which city did you grow up in?\n")  
+
+# 사용자에게 반려동물의 이름을 입력받고, 입력값을 변수 'pet'에 저장합니다.
+pet = input("What is the name of a pet?\n")  
+
+# 입력받은 'city'와 'pet'을 결합하여 밴드 이름을 생성하고 출력합니다.
+print("Your band name could be: " + city + " " + pet)  
