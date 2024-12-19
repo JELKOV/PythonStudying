443a0c506127ae67fa020ef06fa416666149b9b2
JAEHO AHN <ajh4234@gmail.com>
2024-12-19 15:34:27 +0900

    Day15 : Coffee Machine Project init file

--- Day 15/Coffee Machine Project/main.py
+++ Day 15/Coffee Machine Project/main.py
@@ -1,15 +1,15 @@
 MENU = {
     "espresso": {
         "ingredients": {
-            "water": 50,
-            "coffee": 18,
+            "water": 50,  # 필요한 물 양 (ml)
+            "coffee": 18,  # 필요한 커피 양 (g)
         },
-        "cost": 1.5,
+        "cost": 1.5,  # 가격 ($)
     },
     "latte": {
         "ingredients": {
             "water": 200,
-            "milk": 150,
+            "milk": 150,  # 필요한 우유 양 (ml)
             "coffee": 24,
         },
         "cost": 2.5,
@@ -24,9 +24,12 @@ MENU = {
     }
 }
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
+# 수익 초기화
+profit = 0
 
+# 머신의 현재 재료 상태
+resources = {
+    "water": 300,  # 물의 초기 양 (ml)
+    "milk": 200,  # 우유의 초기 양 (ml)
+    "coffee": 100,  # 커피의 초기 양 (g)
+}
